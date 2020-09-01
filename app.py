df96167426ccab489dbb2e6fac6ad914643caf8a
leeminze214 <leeminz214@gmail.com>
2020-09-01 17:57:06 -0400

    'restart'

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from db_class import config, methods
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'asdkjhl2938h82h43ph9f0h8(*&go9d8fg'
-io = SocketIO(app)
+socketio = SocketIO(app)
 execute = methods(config) #database methods and connection 
 
 
@@ -12,6 +12,7 @@ execute = methods(config) #database methods and connection
 #----------------------------------routes-------------------------------#
 
 #login and signup page as default
+
 @app.route('/', methods = ["GET","POST"])
 def login():
     form = request.form.to_dict()
@@ -58,15 +59,16 @@ def signup():
 #after user logs in, they will be led to main lobby where they can join diff chatrooms
 @app.route('/lobby')
 def main_lobby():
+   
     return render_template('lobby.html', name = session['username'])
 
 #returns respective topic's chatroom, using socket.io rooms
 @app.route('/lobby/<topic>')
 def activity(topic):
     activities = ['basketball','swimming','hiking','soccer','camping']
-
+    
     if topic in activities:
-
+      
         return render_template('chat.html',topic = topic, name = session['username'])
 
     return abort(404, 'No chatroom found on topic')
@@ -84,30 +86,5 @@ def logout():
 
 #------------------------------------socket-------------------------------#
 
-@io.on('connected')
-def conn(data):
-  
-    room = data['room']
-    join_room(room)
-    
-    print(room)
-    print('a client ' + session['username']+' just joined room '+room ) 
- 
-    io.send(session['username']+' joined', room = room)
-
-@io.on('disconnect')
-def diss():
-    leave_room()
-    io.emit('disconnected')
-    print('a client '+session['username']+' just left')
-
-@io.on('message_sent')
-def chatting(message_info):
-    user_name =message_info['user_name']
-    message_content =  message_info['message_val']
-    room = message_info['room']
-    print('A message has been sent: ',message_info)
-    io.send(user_name+': '+message_content, room = room)
-
 if __name__ == '__main__':
-    io.run(app, debug = True)
+    socketio.run(app, debug = True)
